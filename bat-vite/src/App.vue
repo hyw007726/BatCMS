b901985cfc239b161dd1cdd4a5dcf982fb7642ce
Wesley Han <hyw007726@hotmail.com>
2022-05-03 18:04:57 +0800

    5.3

--- bat-vite/src/App.vue
+++ bat-vite/src/App.vue
@@ -12,12 +12,14 @@
 </template>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
+@import "./assets/css/main.css";
+@import "./assets/css/color-dark.css";
 </style>
--- bat-vite/src/router/index.js
+++ bat-vite/src/router/index.js
@@ -1,6 +1,20 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import Home from "../views/Home.vue";
-const routes = []
+const routes = [
+    //     {
+    //     path: '/',
+    //     redirect: '/dashboard'
+    // },
+    {
+        // path: "/login",
+        path: "/",
+        name: "Login",
+        meta: {
+            title: '登录'
+        },
+        component: () => import( /* webpackChunkName: "login" */ "../views/Login.vue")
+    }
+]
 
 const router = createRouter({
     history: createWebHashHistory(),
