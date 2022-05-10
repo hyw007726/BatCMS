c55963273716a66ac3bb9018694d5d2493d74fc6
Wesley Han <hyw007726@hotmail.com>
2022-05-10 22:32:50 +0800

    Echarts imported;todo:change id to ref;try display users data table

--- bat-vite/src/App.vue
+++ bat-vite/src/App.vue
@@ -2,6 +2,9 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 // import HelloWorld from './components/HelloWorld.vue'
+import * as echarts from "echarts";
+import { provide } from "vue";
+provide("ec", echarts);
 </script>
 
 <template>
--- bat-vite/src/main.js
+++ bat-vite/src/main.js
@@ -6,6 +6,7 @@ import 'element-plus/dist/index.css'
 import './assets/css/icon.css'
 import { createPinia } from 'pinia'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+
 const app = createApp(App)
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
     app.component(key, component)
