10330fe32df089cefb82122c6d0b1464803b5932
Wesley Han <hyw007726@hotmail.com>
2022-05-22 22:44:30 +0800

    5.22

--- bat-vite/src/views/UserData.vue
+++ bat-vite/src/views/UserData.vue
@@ -1,6 +1,89 @@
 <template>
   <div>
     <!-- {{ data }} -->
+    <el-table
+      :data="tableData"
+      border
+      class="table"
+      ref="multipleTable"
+      header-cell-class-name="table-header"
+    >
+      <el-table-column
+        prop="id"
+        label="ID"
+        width="55"
+        align="center"
+      ></el-table-column>
+      <!-- <el-table-column prop="open_id" label="openid"></el-table-column>
+      <el-table-column prop="union_id" label="unionid"></el-table-column> -->
+      <el-table-column prop="nickname" label="用户名"></el-table-column>
+      <el-table-column prop="sex" label="性别"></el-table-column>
+      <el-table-column label="头像(查看大图)" align="center">
+        <template #default="user">
+          <el-image
+            class="table-td-thumb"
+            :src="user.row.avatar"
+            :preview-src-list="[user.row.avatar]"
+          >
+          </el-image>
+        </template>
+      </el-table-column>
+      <el-table-column label="操作" width="180" align="center">
+        <template #default="scope">
+          <el-button type="text" icon="edit" @click="changeSex()"
+            >改变性别
+          </el-button>
+        </template>
+      </el-table-column>
+      <!-- <el-table-column prop="name" label="用户名"></el-table-column>
+        <el-table-column label="账户余额">
+          <template #default="scope">￥{{ scope.row.money }}</template>
+        </el-table-column>
+        <el-table-column label="头像(查看大图)" align="center">
+          <template #default="scope">
+            <el-image
+              class="table-td-thumb"
+              :src="scope.row.thumb"
+              :preview-src-list="[scope.row.thumb]"
+            >
+            </el-image>
+          </template>
+        </el-table-column>
+        <el-table-column prop="address" label="地址"></el-table-column>
+        <el-table-column label="状态" align="center">
+          <template #default="scope">
+            <el-tag
+              :type="
+                scope.row.state === '成功'
+                  ? 'success'
+                  : scope.row.state === '失败'
+                  ? 'danger'
+                  : ''
+              "
+              >{{ scope.row.state }}</el-tag
+            >
+          </template>
+        </el-table-column>
+
+        <el-table-column prop="date" label="注册时间"></el-table-column>
+        <el-table-column label="操作" width="180" align="center">
+          <template #default="scope">
+            <el-button
+              type="text"
+              icon="edit"
+              @click="handleEdit(scope.$index, scope.row)"
+              >编辑
+            </el-button>
+            <el-button
+              type="text"
+              icon="delete"
+              class="red"
+              @click="handleDelete(scope.$index, scope.row)"
+              >删除</el-button
+            >
+          </template>
+        </el-table-column> -->
+    </el-table>
   </div>
 </template>
 <script setup>
@@ -8,13 +91,18 @@ import { ref, reactive } from "vue";
 import { ElMessage, ElMessageBox } from "element-plus";
 import axios from "axios";
 var offset = 0;
+var tableData = ref([]);
 // axios.get("http://jsonplaceholder.typicode.com/posts")
 axios
   .get("http://www.batenglish.cn:3000/member", { params: { offset: offset } })
   .then((res) => {
     console.log(res.data);
     offset = offset + res.data.length;
+    tableData.value = res.data;
   });
+const changeSex = () => {
+  alert("changeSex");
+};
 const labels = {
   0: "id",
   1: "open_id",
